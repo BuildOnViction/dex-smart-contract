9ca2619667642c3dcabb0a46a9d6260608095cfe
Hoang Trinh <hoang.trinhj@gmail.com>
2019-01-17 11:46:26 +0700

    Fix ESLint Issues And Remove Redundant Code

--- config/index.js
+++ config/index.js
@@ -1,41 +1,27 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const contractAddresses = require('./contractAddresses.json');
+const contractAddresses = require('./contractAddresses.json')
 
 // const quoteTokens = ['WETH', 'DAI', 'TUSD', 'USDC'];
-const quoteTokens = ['WETH', 'DAI'];
+const quoteTokens = ['WETH', 'DAI']
 const baseTokens = [
   'AE',
   'BAT',
-  // 'BNB',
-  // 'GNT',
-  // 'KNC',
-  // 'LOOM',
-  // 'LRC',
-  // 'MITH',
-  // 'MKR',
-  // 'NPXS',
-  // 'OMG',
-  // 'PRFT',
-  // 'REP',
-  // 'SNT',
-  // 'WTC',
-  'ZRX'
-];
-
-const makeFees = {
-  WETH: '0',
-  DAI: '0',
-  TUSD: '0',
-  USDC: '0'
-};
-
-const takeFees = {
-  WETH: '0',
-  DAI: '0',
-  TUSD: '0',
-  USDC: '0'
-};
+    // 'BNB',
+    // 'GNT',
+    // 'KNC',
+    // 'LOOM',
+    // 'LRC',
+    // 'MITH',
+    // 'MKR',
+    // 'NPXS',
+    // 'OMG',
+    // 'PRFT',
+    // 'REP',
+    // 'SNT',
+    // 'WTC',
+  'ZRX',
+]
 
 const decimals = {
   AE: 18,
@@ -57,13 +43,13 @@ const decimals = {
   WTC: 18,
   ZRX: 18,
   TUSD: 18,
-  USDC: 6
-};
+  USDC: 6,
+}
 
 const operatorAddresses = {
   '1': [],
-  '8888': ['0x6e6bb166f420ddd682caebf55dafbafda74f2c9c']
-};
+  '8888': ['0x6e6bb166f420ddd682caebf55dafbafda74f2c9c'],
+}
 
 const testAccounts = [
   '0xcc5697310277bcc3be506f53ed8aafc9d17a2c18',
@@ -75,16 +61,16 @@ const testAccounts = [
   '0xbf8e9e3f9dbb85554679ce8147077b0496358f53',
   '0xc8b74b6b883a96e3defd62934ec3a1e44f149860',
   '0x53ee745b3d30d692dc016450fef68a898c16fa44',
-  '0xe0a1240b358dfa6c167edea09c763ae9f3b51ea0'
-];
+  '0xe0a1240b358dfa6c167edea09c763ae9f3b51ea0',
+]
 
 const keys = {
   '1': (process.env.TOMO_MAINNET_KEYS || '').split(','),
   '4': (process.env.TOMO_RINKEBY_KEYS || '').split(','),
-  '8888': []
-};
+  '8888': [],
+}
 
-let tokenContracts = null;
+let tokenContracts = null
 const getTokenContracts = (artifacts, filters) => {
   if (tokenContracts === null) {
     tokenContracts = [
@@ -111,32 +97,30 @@ const getTokenContracts = (artifacts, filters) => {
       artifacts.require('../contracts/tokens/KNC.sol'),
       artifacts.require('../contracts/tokens/LOOM.sol'),
       artifacts.require('../contracts/tokens/PRFT.sol'),
-      artifacts.require('../contracts/tokens/DAI.sol')
-    ];
+      artifacts.require('../contracts/tokens/DAI.sol'),
+    ]
   }
-  if (!filters) return tokenContracts;
+  if (!filters) return tokenContracts
   return tokenContracts.filter(tokenContract =>
-    filters.includes(tokenContract.contractName)
-  );
-};
+        filters.includes(tokenContract.contractName),
+    )
+}
 
 module.exports = {
   quoteTokens,
   baseTokens,
   tokens: [...baseTokens, ...quoteTokens],
-  takeFees,
-  makeFees,
   decimals,
   operatorAddresses,
   testAccounts,
   contractAddresses,
   keys,
-  // truffle config
+    // truffle config
   infura: {
     ethereum: 'https://mainnet.infura.io/Oi27hEUIuGqMsrYGpI7e',
     ropsten: 'https://ropsten.infura.io/Oi27hEUIuGqMsrYGpI7e',
     rinkeby: 'https://rinkeby.infura.io/Oi27hEUIuGqMsrYGpI7e',
-    kovan: 'https://kovan.infura.io/Oi27hEUIuGqMsrYGpI7e'
+    kovan: 'https://kovan.infura.io/Oi27hEUIuGqMsrYGpI7e',
   },
   constants: {
     DEFAULT_GAS: 4.5 * 10 ** 6,
@@ -147,18 +131,18 @@ module.exports = {
     TOKENS_ALLOCATED_TO_PROOF: 1181031 * 10 ** 18,
     DECIMALS_POINTS: 10 ** 18,
     TOKEN_UNITS: 10 ** 18,
-    ETHER: 10 ** 18
+    ETHER: 10 ** 18,
   },
   ipfs: {
     PRODUCTION: 'QmUM5eT3vY9NfeJ9JBg8xcrom3jEZ3J86wRtNPsFBJ6QFo',
     TESTING_SUCCESS: 'QmVuyH2JtbTguZuC5HsefNBbu8SNSUdq6mrHHDhGeLob3V',
-    TESTING_FAIL: 'QmUGo9mjcdc232p4YUgGf5mdZ5aAvVrVpp8AXiN4AuvBZd'
+    TESTING_FAIL: 'QmUGo9mjcdc232p4YUgGf5mdZ5aAvVrVpp8AXiN4AuvBZd',
   },
   accounts: {
     development: ['0x6e6bb166f420ddd682caebf55dafbafda74f2c9c'],
     rinkeby: [],
     ropsten: [],
-    ethereum: []
+    ethereum: [],
   },
-  getTokenContracts
-};
+  getTokenContracts,
+}
