0a9c71d15fe78c737ed2be33d84c0b8bbeec5515
Hoang Trinh <hoang.trinhj@gmail.com>
2019-01-19 19:28:34 +0700

    Fix ESLint Issues

--- truffle.js
+++ truffle.js
@@ -3,8 +3,8 @@ require('babel-register')
 
 // Allows us to use ES6 in our migrations and tests.
 require('dotenv').config()
-var config = require('./config')
-var secret = require('./secret-config')
+const config = require('./config')
+const secret = require('./secret-config')
 
 require('babel-register')
 require('babel-polyfill')
@@ -19,7 +19,7 @@ module.exports = {
       network_id: '8888',
       gas: config.constants.MAX_GAS,
       gasPrice: config.constants.DEFAULT_GAS_PRICE,
-      from: '0x28074f8D0fD78629CD59290Cac185611a8d60109' // testprc main account here
+      from: '0x28074f8D0fD78629CD59290Cac185611a8d60109', // testprc main account here
     },
     development_geth: {
       host: 'localhost',
@@ -27,44 +27,44 @@ module.exports = {
       network_id: '8888',
       gas: config.constants.MAX_GAS,
       gasPrice: config.constants.DEFAULT_GAS_PRICE,
-      from: '0x6e6BB166F420DDd682cAEbf55dAfBaFda74f2c9c' // testprc main account here
+      from: '0x6e6BB166F420DDd682cAEbf55dAfBaFda74f2c9c', // testprc main account here
     },
     ethereum: {
       provider: new LightWalletProvider({
         keystore: secret.ethereum.keystore,
         password: secret.ethereum.password,
-        rpcUrl: config.infura.ethereum
+        rpcUrl: config.infura.ethereum,
       }),
       network_id: '1',
       gas: config.constants.MAX_GAS,
-      gasPrice: config.constants.DEFAULT_GAS_PRICE
+      gasPrice: config.constants.DEFAULT_GAS_PRICE,
     },
     ropsten: {
       provider: new LightWalletProvider({
         keystore: secret.ropsten.keystore,
         password: secret.ropsten.password,
-        rpcUrl: config.infura.ropsten
+        rpcUrl: config.infura.ropsten,
       }),
       gas: config.constants.MAX_GAS,
       gasPrice: config.constants.DEFAULT_GAS_PRICE,
-      network_id: '3'
+      network_id: '3',
     },
     rinkeby: {
       provider: new LightWalletProvider({
         keystore: secret.rinkeby.keystore,
         password: secret.rinkeby.password,
-        rpcUrl: config.infura.rinkeby
+        rpcUrl: config.infura.rinkeby,
       }),
       gas: config.constants.MAX_GAS,
       gasPrice: config.constants.DEFAULT_GAS_PRICE,
-      network_id: '4'
+      network_id: '4',
     },
     coverage: {
       host: 'localhost',
       network_id: '*',
       port: 8555,
       gas: 0xfffffffffff,
-      gasPrice: 0x01
-    }
-  }
+      gasPrice: 0x01,
+    },
+  },
 }
