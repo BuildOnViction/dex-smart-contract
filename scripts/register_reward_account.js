1362acbe4b4c8cf97be55cb28e5f047818dc2d98
Hoang Trinh <hoang.trinhj@gmail.com>
2019-01-22 11:59:30 +0700

    Update Script To Register Reward Account

--- scripts/register_reward_account.js
+++ scripts/register_reward_account.js
@@ -3,7 +3,7 @@ const path = require('path')
 const argv = require('yargs').argv
 const { utils, providers, Wallet, Contract } = require('ethers')
 const { Exchange } = require('../utils/abis')
-const { contractAddresses, operatorAddresses } = require('../config')
+const { contractAddresses, rewardAddresses } = require('../config')
 const { getNetworkID, getPrivateKeyFromEnvironment, getProvider } = require('../utils/helpers')
 
 const network = argv.network
@@ -17,15 +17,19 @@ const provider = getProvider(network)
 const signer = new Wallet(pk, provider)
 
 const setRewardAccount = async () => {
-  const rewardAccount = '0xDfAb934D4BfA111e3C29D864c36e474880CE0705'
-  const exchange = new Contract(addresses['Exchange'], Exchange, signer)
-  const tx = await exchange.setRewardAccount(rewardAccount)
-  const receipt = await signer.provider.waitForTransaction(tx.hash)
+  try {
+    const rewardAccount = rewardAddresses[networkID]
+    const exchange = new Contract(addresses['Exchange'], Exchange, signer)
+    const tx = await exchange.setRewardAccount(rewardAccount)
+    const receipt = await signer.provider.waitForTransaction(tx.hash)
 
-  if (receipt.status === 0) {
-    console.log(`Transaction ${tx.hash} failed`)
-  } else {
-    console.log(`${rewardAccount} is now the reward account`)
+    if (receipt.status === 0) {
+      console.log(`Transaction ${tx.hash} failed`)
+    } else {
+      console.log(`${rewardAccount} is now the reward account`)
+    }
+  } catch (err) {
+    console.log(err)
   }
 }
 
