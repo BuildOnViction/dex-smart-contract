031ffebb58e39dc9c07edaed0beb8d846c72f240
Hoang Trinh <hoang.trinhj@gmail.com>
2019-03-05 14:59:56 +0700

    Remove Some Unused Tokens For Now

--- config/index.js
+++ config/index.js
@@ -29,9 +29,9 @@ const getTokenContracts = (artifacts, filters) => {
 module.exports = {
   NETWORK_ID,
   tokens: [
-    'BTC',
+    // 'BTC',
     'ETH',
-    'USDT',
+    // 'USDT',
   ],
   rewardAddresses,
   // truffle config
